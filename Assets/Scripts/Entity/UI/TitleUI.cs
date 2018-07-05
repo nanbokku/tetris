faeae2545971fa4382e25998321824e07bd37465
YukaNanboku <yukawp516@gmail.com>
2018-07-05 23:16:19 +0900

    add: TitleUIの追加．

---
+++ Assets/Scripts/Entity/UI/TitleUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUI : MonoBehaviour
+{
+    public System.Action OnStartKeyPressed { get; set; }
+
+    [SerializeField]
+    private Text guideTxt;
+
+    private float oldTime;
+    private bool on;
+
+    private const float intervalTime = 0.5f;
+
+
+    void OnEnable()
+    {
+        oldTime = Time.time;
+        on = true;
+    }
+
+    void Update()
+    {
+        // 点滅するテキストの更新
+        if (Time.time - oldTime > intervalTime)
+        {
+            oldTime = Time.time;
+
+            on = !on;
+            guideTxt.gameObject.SetActive(on);
+        }
+
+        // 入力の確認
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnStartKeyPressed();
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/Controller/TitleUIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUIController : MonoBehaviour
+{
+    public System.Action OnStarted { get; set; }
+
+    private TitleUI titleUI;
+
+
+    void Awake()
+    {
+        titleUI = GetComponentInChildren<TitleUI>();
+    }
+
+    void Start()
+    {
+        titleUI.OnStartKeyPressed = () =>
+        {
+            OnStarted();
+        };
+    }
+}
